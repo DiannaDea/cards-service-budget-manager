865ffbdc57530b5a7618a77eb45ad3793c8f2f22
Diana Baburina <dbaburina.consultant@newscorp.com>
2020-05-16 17:22:37 +0300

    Fix

--- groups.js
+++ groups.js
@@ -1,13 +1,13 @@
 module.exports = {
-  1: {
+  'aa9db47c-9aaa-4f64-97a9-f1b5fe118b7b': {
     id: 'aa9db47c-9aaa-4f64-97a9-f1b5fe118b7b',
     name: 'Private',
   },
-  2: {
+  'fe69984c-7d35-41e0-925d-45270e7fae99': {
     id: 'fe69984c-7d35-41e0-925d-45270e7fae99',
     name: 'Family',
   },
-  3: {
+  '9cdd2afd-7245-440e-9b85-4c4297d8f687': {
     id: '9cdd2afd-7245-440e-9b85-4c4297d8f687',
     name: 'Business',
   },
